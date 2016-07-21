4d9a2112b22d56787a3e6e679a72695306f8bdaa
Tristan Palmer <chesswizard74@gmail.com>
2016-07-21 17:28:37 +0000

    404 is almost done, but broken

--- 404Page/404Page.js
+++ 404Page/404Page.js
@@ -1,2 +1,36 @@
-    var sc = document.getElementById("seconds");
-    var sctx = sc.getContext("2d");
+ var ranNum = Math.random * 10
+  
+   if(ranNum < 4){
+    var tree = document.getElementById("window");
+    var img1 = tree.getContext("2d");
+    window.onload = function() {
+    var img = document.getElementById("forest");
+   img1.drawImage(img, 0, 0);
+   };
+   }else if(ranNum >= 4){
+    var moon = document.getElementById("window2");
+    var img2 = moon.getContext("2d");
+    window.onload = function() {
+    var img = document.getElementById("Space");
+    img2.drawImage(img, 0, 0);
+    };}
+    else{
+    var sand = document.getElementById("window3");
+    var img3 = sand.getContext("2d");
+    window.onload = function() {
+    var img = document.getElementById("desert");
+    img3.drawImage(img, 0, 0);
+    };
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
