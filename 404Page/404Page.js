0f907d7686384d05939c14ebd461c4488526f673
Tristan Palmer <chesswizard74@gmail.com>
2016-07-26 15:11:35 +0000

    Fixed the 404 need to style

--- 404Page/404Page.js
+++ 404Page/404Page.js
@@ -1,36 +1,26 @@
- var ranNum = Math.random * 10
-  
-   if(ranNum < 4){
-    var tree = document.getElementById("window");
-    var img1 = tree.getContext("2d");
-    window.onload = function() {
-    var img = document.getElementById("forest");
+ var ranNum = Math.random() * 10;
+ if (ranNum <= 3) {
+  var tree = document.getElementById("window");
+  var img1 = tree.getContext("2d");
+  window.onload = function() {
+   var img = document.getElementById("forest");
    img1.drawImage(img, 0, 0);
-   };
-   }else if(ranNum >= 4){
-    var moon = document.getElementById("window2");
-    var img2 = moon.getContext("2d");
-    window.onload = function() {
-    var img = document.getElementById("Space");
-    img2.drawImage(img, 0, 0);
-    };}
-    else{
-    var sand = document.getElementById("window3");
-    var img3 = sand.getContext("2d");
-    window.onload = function() {
-    var img = document.getElementById("desert");
-    img3.drawImage(img, 0, 0);
-    };
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+  };
+ }
+ else if (ranNum <= 6) {
+  var moon = document.getElementById("window");
+  var img2 = moon.getContext("2d");
+  window.onload = function() {
+   var img = document.getElementById("Space");
+   img2.drawImage(img, 0, 0);
+  };
+
+ }
+ else {
+  var sand = document.getElementById("window");
+  var img3 = sand.getContext("2d");
+  window.onload = function() {
+   var img = document.getElementById("desert");
+   img3.drawImage(img, 0, 0);
+  };
+ }
